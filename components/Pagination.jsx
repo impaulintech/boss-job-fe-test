397616482cb41cfdd64715f97d4ad406e67c95d2
ImPaulinTech <impaulintech@gmail.com>
2022-04-04 22:17:23 +0800

    pagination done. next; redux saga connection

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,42 +1,61 @@
 import PaginationStyles from "../styles/Pagination.module.css";
+import { useState } from 'react';
 
 export const Pagination = () => {
   const ps = PaginationStyles;
-  const page = 5;
-  const paginate = page >= 5 ? [1, 2, 3, 4, 5] : [6, 7, 8, 9, 10];
+
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const lastPage = currentPage + 3
+  const firstPage = lastPage - 4
+
+  const sliceStart = currentPage >= arr.length - 4 ? arr.length - 5 : firstPage
+  const sliceEnd = currentPage >= arr.length - 4 ? arr.length - 1 : lastPage + 1
+
   return (
     <>
       <div className={ps.paginationContainer}>
         <ul>
-          <li>
+
+          <li >
             <a href="#">{"<"}</a>
           </li>
-          {page >= 5 ? null : (
-            <>
-              <li>
-                <a href="#">1</a>
+
+          {[arr[0], "..."].map((value) => {
+            return currentPage >= 3
+              ? <li key={value}>
+                <a href="#">{value}</a>
               </li>
-              <li>...</li>
-            </>
-          )}
-          {paginate.map((num) => {
-            return (
-              <li key={num}>
-                <a href="#">{num}</a>
-              </li>
-            );
+              : null
           })}
-          {page <= 4 ? null : (
-            <>
-              <li>...</li>
-              <li>
-                <a href="#">44</a>
+
+          {arr.slice(sliceStart, sliceEnd).map((num) => {
+
+            return <li key={Math.random()}>
+              <a href="#">{num}</a>
+            </li>
+
+
+          })}
+
+          {["...", arr.length + 1].map((value) => {
+            return currentPage <= arr.length - 6
+              ? value === "..." ? <li key={value}>
+                <a href="#">{value}</a>
+              </li> : <li key={value}>
+                <a href="#">{value - 1}</a>
               </li>
-            </>
-          )}
-          <li style={{ color: "var(--color-primary)" }}>
+              : value === "..." ? null : <li key={value}>
+                <a href="#">{value - 1}</a>
+              </li>
+          })}
+
+          <li>
             <a href="#">{">"}</a>
           </li>
+
+
         </ul>
       </div>
     </>
